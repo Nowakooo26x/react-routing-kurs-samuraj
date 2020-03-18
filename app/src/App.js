cbe1904995b759f208a7d62656057d2c7a598c0d
Nowakooo26x <nowakooo26x@gmail.com>
2020-03-18 13:47:40 +0100

    v5

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Link} from 'react-router-dom';
+
+//Komponent - Home
+const Home = () => <h1>Strona startowa</h1>;
+//Komponent - News
+const News = () => <h1>Strona news</h1>;
+//Komponent - Contact
+const Contact = () => <h1>Strona contact</h1>;
+
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <heder>
         <nav>
           <ul>
+            {/*
             <li><a href="/">start</a></li>
             <li><a href="/news">news</a></li>
             <li><a href="/contact">contact</a></li>
+            */}
+            <li><Link to="/">start</Link></li>
+            <li><Link to="/news">news</Link></li>
+            <li><Link to="/contact">contact</Link></li>
+            
+
           </ul>
         </nav>
       </heder>
       <section>
-        Strona - witaj!
+        <Home/>
+        <News/>
+        <Contact/>
       </section>
     </div>
+    </BrowserRouter>
   );
 }
 
